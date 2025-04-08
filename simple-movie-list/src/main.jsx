ece5c309a049b1b000da3d1da1a7a6a8099cf812
Rafif <merafif28@gmail.com>
2025-04-08 18:32:21 +0700

    Menambahkan star rating

--- simple-movie-list/src/main.jsx
+++ simple-movie-list/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import StarRating from './StarRating.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating max={5}/>
+    <StarRating max={10}/>
   </StrictMode>,
 )
